6bec0d866619a7b95345e88fec6b3a66deb3db0c
vinicius <vinicius@vinicius.local>
2026-01-29 22:57:11 -0300

    :feat Added Auth feature

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'static.home')->name('home-page');
+Route::view('/', 'static.home')->name('static.home');
+
+Route::middleware('guest')->group(function (): void {
+    Route::livewire('/signin', 'auth.login')->name('login');
+});
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
 |--------------------------------------------------------------------------
 | Test Case
@@ -12,8 +14,8 @@
 */
 
 pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->in('Feature', '../resources/views');
 
 /*
 |--------------------------------------------------------------------------
