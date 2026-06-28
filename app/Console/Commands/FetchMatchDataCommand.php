de70b48e4d273621972f1ef9dbb1564fa58bb542
Vinicius <vinicius@promofarma.com.br>
2026-06-28 13:48:40 -0300

    :feat Added command for sync matches

--- app/Console/Commands/FetchMatchDataCommand.php
+++ app/Console/Commands/FetchMatchDataCommand.php
@@ -4,50 +4,60 @@ declare(strict_types=1);
 
 namespace App\Console\Commands;
 
-use App\Enums\Fixture\Status;
-use App\Models\Competition;
-use App\Services\Fixture\SyncScore;
+use App\Models\Fixture;
+use App\Models\Team;
 use App\Services\FootballData\CompetitionMatch;
+use Carbon\CarbonImmutable;
 use Illuminate\Console\Command;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+use Throwable;
 
 final class FetchMatchDataCommand extends Command
 {
-    protected $signature = 'app:fetch-matches {year?}';
+    protected $signature = 'app:fetch-matches {competition} {season} {stage}';
 
-    protected $description = 'Fetch competition matches';
+    protected $description = 'Fetch competition matches by Competition Id, Season Year and Stage.';
 
-    public function handle(CompetitionMatch $service)
+    public function handle(CompetitionMatch $service): int
     {
-        $competitions = Competition::query()
-            ->whereHas('seasons', function (Builder $query): void {
-                $query->whereHas('fixtures', function (Builder $query) {
-                    $query->whereNot('status', Status::Finished);
-                });
-            })
-            ->whereNotNull('external_id')
-            ->get([
-                'id',
-                'external_id',
+        try {
+            $data = $service->matchesByCompetitionId((int) $this->argument('competition'), [
+                'season' => (int) $this->argument('season'),
+                'stage' => $this->normalizeStage($this->argument('stage')),
             ]);
 
-        foreach ($competitions as $competition) {
-            $response = $service->matchesByCompetitionId(
-                id: $competition->external_id,
-                query: [
-                    'season' => $this->argument('year') ?? today()->year,
-                ],
-            );
+            $matches = $data['matches'] ?? [];
 
-            if (! is_array($response)) {
-                continue;
+            foreach($matches as $match) {
+                $matchDate = CarbonImmutable::parse($match['utcDate'])->subHours(3);
+
+                Fixture::query()
+                    ->where('external_id', $match['id'])
+                    ->update([
+                        'home_team_id' => $this->resolveTeam($match['homeTeam']['id'])?->id,
+                        'away_team_id' => $this->resolveTeam($match['awayTeam']['id'])?->id,
+                        'match_date'   => $matchDate,
+                        'locked_at'    => $matchDate->subMinutes(30),
+                    ]);
+
+                $this->info('Fixture ['.$match['id'].'] updated successfull');
             }
-
-            Storage::disk('local')->put(
-                path: 'data-'.today()->format('Y-m-d').'.json',
-                contents: json_encode($response['matches'] ?? []),
-            );
+        } catch (Throwable $e) {
+            $this->info('Failed fetch to API');
+            return Command::FAILURE;
         }
+
+        return Command::SUCCESS;
+    }
+
+    private function normalizeStage(string $value): string
+    {
+        return (string) Str::of($value)->snake()->upper()->trim();
+    }
+
+    private function resolveTeam(int $teamId): Team
+    {
+        return Team::query()->where('external_id', $teamId)->first('id');
     }
 }
